1e5fa31db55ca0248651e7ff04159fd3ebe51746
meepo981 <miposigna@gmail.com>
2024-09-27 22:34:52 +0300

    Add files via upload

---
+++ labs 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <math.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, ".1251");
+    int a, b, p, S;
+        cout << "Введiть значення a =";
+        cin >> a; 
+        cout << "Введiть значення b =";
+        cin >> b;
+        S = a * b;
+            p = 2 * (a + b);
+            cout<<"Area = "<<S<<endl<<" Perimetr = " <<p<<endl;
+            int Q, R, P, D;
+            Q = a + b;
+            R = a - b;
+            P = a * b;
+            D = a / b;
+            cout << "Suma = " << Q << endl << "Riznica = " << R << endl << "Produkt = " << P << endl << "Division = " << D << endl;
+            system("pause>>void");
+            return 0;
+}
