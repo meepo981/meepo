73608f825f88a839077a1355d19cdc7b22a099b2
meepo981 <miposigna@gmail.com>
2024-09-27 22:56:16 +0300

    Add files via upload

---
+++ task 5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	setlocale(0, ".1251");
+	int a;
+	cout << "Ваша оцiнка: ";
+	cin >> a;
+	if (a > 0 && a < 50) cout << "Незадовiльно";
+	else if (a >= 50 && a < 70) cout << "Задовiльно";
+	else if (a >= 70 && a < 90) cout << "Добре";
+	else if (a >= 90 && a <= 100) cout << "Вiдмiнно";
+	else {
+		cout << "Не вірне значення" << endl;
+	}
+	return 0;	
+}
